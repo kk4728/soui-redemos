0a42ef037ee58763f24165e3462a44d69e573711
kk4728 <gongrong816@126.com>
2019-01-19 11:56:56 +0800

    add cumstom ctrl of SMyCtrl.

--- WizardDemo/SMyCtrl.cpp
+++ WizardDemo/SMyCtrl.cpp
@@ -6,6 +6,7 @@ namespace SOUI {
 
 SMyCtrl::SMyCtrl(void)
 {
+	m_bFocusable = 1;
 }
 
 
@@ -15,12 +16,59 @@ SMyCtrl::~SMyCtrl(void)
 
 void SMyCtrl::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
 {
-
+	m_strInput.Append((wchar_t)nChar);
+	Invalidate();
 }
 
 void SMyCtrl::OnPaint(IRenderTarget *pRT)
 {
+	SPainter painter;
+	BeforePaint(pRT, painter);
 
+	CRect rcClient = GetClientRect();
+	if(IsFocusable())
+	{
+		CRect rcFocus = rcClient;
+		rcFocus.DeflateRect(2,2);
+		DrawDefFocusRect(pRT, rcFocus);
+		
+	}
+	SStringT strText = S_CW2T(m_strInput);
+	pRT->TextOut(rcClient.left, rcClient.top, strText, strText.GetLength());
+
+	if(m_lstClick.GetCount() >= 2) {
+		CPoint pts[2] = { m_lstClick[0] + rcClient.TopLeft() };
+		for (int i = 1; i<m_lstClick.GetCount(); i++)
+		{
+			pts[1] = m_lstClick[i] + rcClient.TopLeft();
+			pRT->DrawLines(pts, 2);
+			pts[0] = pts[1];
+		}
+	} 
+	else if(m_lstClick.GetCount() == 1)
+	{
+		CPoint pt = m_lstClick[0] + rcClient.TopLeft();
+		CRect rc(pt, CSize(8, 8));
+		rc.OffsetRect(-4,-4);
+		pRT->DrawEllipse(rc);
+	}
+
+	AfterPaint(pRT, painter);
+}
+
+void SMyCtrl::OnChar(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+
+}
+
+void SMyCtrl::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	__super::OnLButtonDown(nFlags, point);
+	CRect rcClient = GetClientRect();
+	CPoint pt1 = point-rcClient.TopLeft();
+	
+	m_lstClick.Add(pt1);
+	Invalidate();
 }
 
 }
--- WizardDemo/SMyCtrl.h
+++ WizardDemo/SMyCtrl.h
@@ -20,12 +20,19 @@ protected:
 
 	void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
 	void OnPaint(IRenderTarget *pRT);
+	void OnChar(UINT nChar, UINT nRepCnt, UINT nFlags);
+	void OnLButtonDown(UINT nFlags, CPoint point);
 
 	SOUI_MSG_MAP_BEGIN()
+		MSG_WM_CHAR(OnChar)
 		MSG_WM_KEYDOWN(OnKeyDown)
-		MSG_WM_PAINT_EX(OnPaint);
+		MSG_WM_PAINT_EX(OnPaint)
+		MSG_WM_LBUTTONDOWN(OnLButtonDown)
 	SOUI_MSG_MAP_END()
 
+private:
+	SStringW  m_strInput;
+	SArray<CPoint> m_lstClick;
 };
 
 
